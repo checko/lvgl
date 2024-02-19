886200e924c7e56e37565fdf421bbc8834419d4c
charles.chang <charles.chang@royaltek.com>
2024-02-19 16:22:17 +0800

    use memfs to load font array ok

--- examples/widgets/label/lv_example_label_1.c
+++ examples/widgets/label/lv_example_label_1.c
@@ -4,16 +4,27 @@
 /**
  * Show line wrap, re-color, line align and text scrolling.
  */
+extern unsigned char zfont_bin[];
+extern unsigned int zfont_bin_len;
 void lv_example_label_1(void)
 {
+    lv_fs_path_ex_t mempath;
+    lv_fs_make_path_from_buffer(&mempath,LV_FS_MEMFS_LETTER,zfont_bin,zfont_bin_len);
+
+    lv_font_t *myfont = lv_font_load(&mempath);
+    static lv_style_t style;
+    lv_style_init(&style);
+    lv_style_set_text_font(&style,myfont);
+
     lv_obj_t * label1 = lv_label_create(lv_scr_act());
     lv_label_set_long_mode(label1, LV_LABEL_LONG_WRAP);     /*Break the long lines*/
     lv_label_set_recolor(label1, true);                      /*Enable re-coloring by commands in the text*/
-    lv_label_set_text(label1, "#0000ff Re-color# #ff00ff words# #ff0000 of a# label, align the lines to the center "
-                      "and wrap long text automatically.");
+    lv_label_set_text(label1, "右轉");
     lv_obj_set_width(label1, 150);  /*Set smaller width to make the lines wrap*/
     lv_obj_set_style_text_align(label1, LV_TEXT_ALIGN_CENTER, 0);
     lv_obj_align(label1, LV_ALIGN_CENTER, 0, -40);
+
+    lv_obj_add_style(label1,&style,0);
 
     lv_obj_t * label2 = lv_label_create(lv_scr_act());
     lv_label_set_long_mode(label2, LV_LABEL_LONG_SCROLL_CIRCULAR);     /*Circular scroll*/
